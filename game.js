8574599f93b61f403095aa7813adc8da3fd18476
Dustin Gouner <dustingouner20@gmail.com>
2023-03-04 20:14:31 -0700

    refac: Make game.js more dynamic, add in images as player tokens

--- game.js
+++ game.js
@@ -1,26 +1,30 @@
 class Game {
   constructor() {
-    this.player1 = new Player(1, 'X')
-    this.player2= new Player(2, 'O')
-    this.playerTurn = 'X'
-    this.firstPlayer = 'X'
+    this.player1 = new Player(1, "./assets/kanye.jpeg")
+    this.player2= new Player(2, "./assets/pete.jpeg")
+    this.playerTurn = this.player1
+    this.firstPlayer = this.player1
     this.turns = 0
     this.activeGame = true
     this.draw = false
     this.winner = null
     
   } 
-  takeTurn() {
-    if (this.playerTurn === 'X') {
-      this.playerTurn = 'O'
-      topMessage.innerText = `It's Player ${thisGame.playerTurn}\'s turn!`
+  takeTurn(boxID) {
+    if (this.playerTurn === this.player1) {
+      this.playerTurn = this.player2
+      this.player1.boardSpot.push(boxID)
+      topMessage.innerHTML = ""
+      topMessage.innerHTML += `<h2>It's<img class="top-token" src="${thisGame.playerTurn.token}" alt="">turn!</h2>`
     } else {
-      this.playerTurn = 'X'
-      topMessage.innerText = `It's Player ${thisGame.playerTurn}\'s turn!`
+      this.playerTurn = this.player1
+      this.player2.boardSpot.push(boxID)
+      topMessage.innerHTML = ""
+      topMessage.innerHTML += `<h2>It's<img class="top-token" src="${thisGame.playerTurn.token}" alt="">turn!</h2>`
     }
+    thisGame.turns += 1
   }
   checkForWinner() {
-    console.log('is this working')
     var waysToWin = [
     [1,2,3],
     [4,5,6],
@@ -38,24 +42,19 @@ class Game {
         this.player1.boardSpot.includes(waysToWin[i][2])) 
         {
         this.player1.wins += 1
-        this.winner = true
+        this.winner = this.player1
         this.activeGame = false
-        topMessage.innerText = 'Kanye Won!'
-       } else if (
+      } else if (
         this.player2.boardSpot.includes(waysToWin[i][0]) &&
         this.player2.boardSpot.includes(waysToWin[i][1]) &&
         this.player2.boardSpot.includes(waysToWin[i][2])
-       ) {
-         this.player2.wins += 1
-         this.winner = true
-         this.activeGame = false
-         topMessage.innerText = 'Pete Won!'
-       }
+      ) {
+        this.player2.wins += 1
+        this.winner = this.player2
+        this.activeGame = false
+      }
     }
     this.checkForDraw()
-  }
-  updateGameBoard(id) {
-    this.gameBoard[id] = this.playerTurn
   }
   checkForDraw() {
     if (this.turns === 9 && this.winner === false) {
@@ -64,17 +63,19 @@ class Game {
     }
   }
   resetGameBoard() {
-    if (this.firstPlayer === 'X') {
-      this.firstPlayer = '0'
-      this.playerTurn = '0'
-    } else if (this.firstPlayer === 'O') {
-      this.firstPlayer = 'X'
-      this.playerTurn = 'X'
+    if (this.playerTurn === this.player1) {
+      this.playerTurn = this.player2
+      topMessage.innerHTML = ""
+      topMessage.innerHTML += `<h2>It's<img class="top-token" src="${thisGame.playerTurn.token}" alt="">turn!</h2>`
+    } else {
+      this.playerTurn = this.player1
+      topMessage.innerHTML = ""
+      topMessage.innerHTML += `<h2>It's<img class="top-token" src="${thisGame.playerTurn.token}" alt="">turn!</h2>`
     }
     this.activeGame = true
     this.draw = false
     this.turns = 0
-    this.winner = false
+    this.winner = null
     this.player1.boardSpot = []
     this.player2.boardSpot = []
   }

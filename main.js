54eb60763d9c3c8ddb68623ea68c54b3d6419e76
Dustin Gouner <dustingouner20@gmail.com>
2023-03-04 20:10:06 -0700

    refac: remove nested if statements in main.js

--- main.js
+++ main.js
@@ -3,37 +3,12 @@ var thisGame = new Game()
 var topMessage = document.querySelector('.top-message')
 var player1Wins = document.querySelector('.player-1-wins')
 var player2Wins = document.querySelector('.player-2-wins')
-var gameBoardContainer = document.querySelector('.box')
-var box1 = document.getElementById('1')
-var box2 = document.getElementById('2')
-var box3 = document.getElementById('3')
-var box4 = document.getElementById('4')
-var box5 = document.getElementById('5')
-var box6 = document.getElementById('6')
-var box7 = document.getElementById('7')
-var box8 = document.getElementById('8')
-var box9 = document.getElementById('9')
-var box =  document.querySelectorAll('.box')
-var image = document.querySelectorAll('.box-token')
+var gameBoardContainer = document.querySelector('.game-board-container')
 
-
-
-var boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9]
 // ----EVENT LISTENERS------
 
 window.addEventListener('load', displayPage)
 gameBoardContainer.addEventListener('click', playGame)
-box1.addEventListener('click', playGame)
-box2.addEventListener('click', playGame)
-box3.addEventListener('click', playGame)
-box4.addEventListener('click', playGame)
-box5.addEventListener('click', playGame)
-box6.addEventListener('click', playGame)
-box7.addEventListener('click', playGame)
-box8.addEventListener('click', playGame)
-box9.addEventListener('click', playGame)
-// box.addEventListener('click', play)
-
 
 // -----FUNCTIONS------
 
@@ -43,62 +18,46 @@ function displayPage() {
   player2Wins.innerHTML = `Wins: ${thisGame.player2.wins}`
 }
 
-
 function playGame(event) {
-  if (thisGame.activeGame) {
-    for (var i = 0; i < boxes.length; i++) {
-      if (thisGame.playerTurn === 'X') {
-        if (boxes[i].id === event.target.id) {
-          boxes[i].innerHTML = ''
-          boxes[i].innerHTML += `<img class="box-token" src="assets/kanye.jpeg" alt="">`
-          updatePlayerOneStats()
-          thisGame.checkForWinner()
-      }
-    } else if (thisGame.playerTurn === 'O') {
-        if (boxes[i].id === event.target.id) {
-        boxes[i].innerHTML = ''
-        boxes[i].innerHTML += `<img class="box-token" src="assets/pete.jpeg" alt="">`
-        updatePlayerTwoStats()
-        thisGame.checkForWinner()
-    }
+  if(event.target.innerHTML === '' && thisGame.winner === null) {
+    var boxID = parseInt(event.target.dataset.indexNumber)
+    event.target.innerHTML += `<img class="box-token" src="${thisGame.playerTurn.token}" alt="">`
+    thisGame.takeTurn(boxID)
+    thisGame.declareWinner()
+  if(thisGame.winner !== null || thisGame.draw) {
+    displayResults()
+    } 
   }
 }
-if (thisGame.winner) {
-  for (var i = 0; i < boxes.length; i++) {
-    boxes[i].innerHTML = ''
+
+function displayResults() {
+  if (thisGame.draw) {
+    topMessage.innerHTML = ""
+    topMessage.innerHTML += "It's a Draw!"
+  } else {
+    topMessage.innerHTML = ""
+    topMessage.innerHTML += `<h2><img class="top-token" src="${thisGame.winner.token}" alt="">WINS!!</h2>`
+    player1Wins.innerText = `Wins: ${thisGame.player1.wins}`
+    player2Wins.innerText = `Wins: ${thisGame.player2.wins}`
   }
-  topMessage.innerText = `${thisGame.playerTurn} wins!`
-  player1Wins.innerText = `Wins: ${thisGame.player1.wins}`
-  player2Wins.innerText = `Wins: ${thisGame.player2.wins}`
+  setTimeout(newGame, 2500)
+}
+
+function newGame() {
+  var gameBoxes = document.querySelectorAll('.box')
+  gameBoxes.forEach(box => box.innerHTML = '')
   thisGame.resetGameBoard()
-} else {
-  thisGame.takeTurn()
-
-    }
-  }
 }
 
 
-
-
-function updatePlayerOneStats() {
-  thisGame.player1.boardSpot.push(parseInt(event.target.id))
-  thisGame.turns += 1
-}
-
-function updatePlayerTwoStats() {
-  thisGame.player2.boardSpot.push(parseInt(event.target.id))
-  thisGame.turns += 1
-}
-
-// function displayGameResult() {
-//   if ()
+// function updatePlayerOneStats() {
+//   thisGame.player1.boardSpot.push(parseInt(event.target.id))
+//   thisGame.turns += 1
 // }
 
-// function clearBoard() {
-
+// function updatePlayerTwoStats() {
+//   thisGame.player2.boardSpot.push(parseInt(event.target.id))
+//   thisGame.turns += 1
 // }
 
 
-// var image = document.getElementById(box-token)
-// image.src = "assets/kanye.jpeg"
